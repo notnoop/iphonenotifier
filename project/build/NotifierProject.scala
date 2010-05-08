d380bf1887beb20e4656acaa9138abccaace5ea9
notnoop <mahmood@notnoop.com>
2010-05-08 02:29:40 -0400

    add rabbitmq client and scalatest as dependencies

--- project/build/NotifierProject.scala
+++ project/build/NotifierProject.scala
@@ -5,10 +5,14 @@ class NotifierProject(info: ProjectInfo) extends DefaultProject(info) {
   val notnoopRepo = "Notnoop Repo" at "http://notnoop.github.com/m2-repo"
   val lagRepo = "Lag Repo" at "http://www.lag.net/repo/"
 
+  val rabbitmq = "com.rabbitmq" % "amqp-client" % "1.7.2"
+
   val javaapns = "com.notnoop.apns" % "apns" % "0.1.4"
   val json_jackson = "org.codehaus.jackson" % "jackson-mapper-asl" % "1.4.0"
 
   val configgy = "net.lag" % "configgy" % "1.5"
   val logback = "ch.qos.logback" % "logback-classic" % "0.9.17"
+
+  val scalatest = "org.scalatest" % "scalatest" % "1.0" % "test"
 }
 
