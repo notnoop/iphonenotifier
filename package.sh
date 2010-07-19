a426a0890cc5917d3d175b088ebb8a9a61b0109e
notnoop <mahmood@notnoop.com>
2010-07-19 00:37:23 -0400

    Adding launching scripts

---
+++ package.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Build
+sbt update compile package
+
+DIST="target/dist"
+
+# Package structure
+rm -rf $DIST
+mkdir -p $DIST/bin $DIST/lib $DIST/conf
+cp lib_managed/scala_2.7.7/compile/* $DIST/lib
+cp target/scala_2.7.7/notifier_2.7.7-1.0.jar $DIST/lib
+
+cp src/main/bash/* $DIST/bin
+chmod +x $DIST/bin/*
